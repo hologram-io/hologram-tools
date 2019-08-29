4e749f9663a51d3bd2b552b480d71311f0cef357
Reuben Balik <rsbalik@gmail.com>
2019-08-29 10:13:22 -0500

    Add final nova410 updater version

--- novaupdate/nova410update.py
+++ novaupdate/nova410update.py
@@ -1,12 +1,24 @@
-# Updater for Nova R410
+# nova410update.py - Updater for the u-blox firmware on Hologram Nova R410
 # Upgrades to Firmware version L05.08A02.04
+#
+# Author: Hologram <support@hologram.io>
+# 
+# Copyright 2019 - Hologram, Inc
+#
+# LICENSE: Distributed under the terms of the MIT License
+#
+# u-blox firmware binaries are Copyright u-blox AG (www.u-blox.com)
 
-from Hologram.HologramCloud import HologramCloud
+
+from Hologram.HologramCloud import HologramCloud, CustomCloud
 import logging
 import os
 import re
+import requests
+import shutil
 import sys
-import urllib.request
+import time
+from xmodem import XMODEM
 import zipfile
 
 class UpdaterException(Exception):
@@ -23,8 +35,8 @@ class NovaR410Updater(object):
         '0200':(
                 ( #package set 0
                     ( #stage 1 possible files
-                        '0bb_stg1_pkg1_0m_L56A0200_to_L58A0204.bin',
-                        '0bb_stg1_pkg2_4m_L56A0200_to_L58A0204.bin ',
+                        '0bb_stg1_pkg1-0m_L56A0200_to_L58A0204.bin',
+                        '0bb_stg1_pkg2_4m_L56A0200_to_L58A0204.bin',
                         '0bb_stg1_pkg3_8m_L56A0200_to_L58A0204.bin'
                     ),
                     ( #stage 2
@@ -34,7 +46,7 @@ class NovaR410Updater(object):
                 ( #package set 1
                     ( #stage 1 possible files
                         '1bb_stg1_pkg1_0m_L56A0200_to_L58A0204.bin',
-                        '1bb_stg1_pkg2_4m_L56A0200_to_L58A0204.bin ',
+                        '1bb_stg1_pkg2_4m_L56A0200_to_L58A0204.bin',
                         '1bb_stg1_pkg3_8m_L56A0200_to_L58A0204.bin'
                     ),
                     ( #stage 2
@@ -44,19 +56,19 @@ class NovaR410Updater(object):
                 ( #package set 2
                     ( #stage 1 possible files
                         '2bb_stg1_pkg1_0m_L56A0200_to_L58A0204.bin',
-                        '2bb_stg1_pkg2_4m_L56A0200_to_L58A0204.bin ',
+                        '2bb_stg1_pkg2_4m_L56A0200_to_L58A0204.bin',
                         '2bb_stg1_pkg3_8m_L56A0200_to_L58A0204.bin'
                     ),
                     ( #stage 2
                         '2bb_stg2_L56A0200_to_L58A0204.bin',
                     )
                 )
-            )
+            ),
         '0201':(
                 ( #package set 0
                     ( #stage 1 possible files
                         '0bb_stg1_pkg1_0m_L56A0201_to_L58A0204.bin',
-                        '0bb_stg1_pkg2_4m_L56A0201_to_L58A0204.bin ',
+                        '0bb_stg1_pkg2_4m_L56A0201_to_L58A0204.bin',
                         '0bb_stg1_pkg3_8m_L56A0201_to_L58A0204.bin'
                     ),
                     ( #stage 2
@@ -66,7 +78,7 @@ class NovaR410Updater(object):
                 ( #package set 1
                     ( #stage 1 possible files
                         '1bb_stg1_pkg1_0m_L56A0201_to_L58A0204.bin',
-                        '1bb_stg1_pkg2_4m_L56A0201_to_L58A0204.bin ',
+                        '1bb_stg1_pkg2_4m_L56A0201_to_L58A0204.bin',
                         '1bb_stg1_pkg3_8m_L56A0201_to_L58A0204.bin'
                     ),
                     ( #stage 2
@@ -76,7 +88,7 @@ class NovaR410Updater(object):
                 ( #package set 2
                     ( #stage 1 possible files
                         '2bb_stg1_pkg1_0m_L56A0201_to_L58A0204.bin',
-                        '2bb_stg1_pkg2_4m_L56A0201_to_L58A0204.bin ',
+                        '2bb_stg1_pkg2_4m_L56A0201_to_L58A0204.bin',
                         '2bb_stg1_pkg3_8m_L56A0201_to_L58A0204.bin'
                     ),
                     ( #stage 2
@@ -85,23 +97,23 @@ class NovaR410Updater(object):
                 )
             )                
         }
-        firmware_url = 'https://ublox-firmware.s3.amazonaws.com/'
+    firmware_url = 'https://ublox-firmware.s3.amazonaws.com/'
 
     def __init__(self):
-        self.logger = logging.getLogger(__name__)
+        self.logger = logging.getLogger('Nova410Updater')
         self.cloud = None
 
     def prompt_for_confirm(self):
+        self.logger.debug('Checking for confirmation')
         prompt = '''
-        You are about to update the UBlox firmware on your Nova R410 device.
-        Once you continue past this point do not power off the modem
-        or stop this utility before it has completed.
-        Note that this update may change some behavior of the modem
-        related to profile selection and LED blinking. See hologram
-        documentation for more information.
-        The update may take up to 20 minutes to apply
-        Do you wish to continue? (y/n)
-        '''
+You are about to update the UBlox firmware on your Nova R410 device.
+Once you continue past this point do not power off the modem
+or stop this utility before it has completed.
+Note that this update may change some behavior of the modem
+related to profile selection and LED blinking. See hologram
+documentation for more information.
+The update may take up to 25 minutes to apply
+Do you wish to continue? (y/n) '''
         reply = str(raw_input(prompt)).lower().strip()
         if reply == 'y':
             return True
@@ -110,39 +122,46 @@ class NovaR410Updater(object):
 
     def download_update_package(self, version):
         # download into ./fwpackage/version and extract
-        filename = 'L0506A%s-to-L0508A0204.zip'%version
+        package = 'L0506A%s-to-L0508A0204'%version
+        filename = package + '.zip'
         dir_path = os.path.dirname(os.path.realpath(__file__))
-        fwdir = os.path.join(dir_path, version)
-        os.mkdir(fwdir)
+        fwdir = os.path.join(dir_path, 'fw', version)
+        if os.path.isdir(fwdir):
+            self.logger.debug('Removing existing firmware directory')
+            shutil.rmtree(fwdir)
+        self.logger.debug('making dir: %s', fwdir)
+        os.makedirs(fwdir)
 
         dl_file = os.path.join(fwdir, filename)
-        firmware_url = self.firmware_url + '/' + filename
-        urllib.request.urlretrieve(firmware_url, dl_file)
+        firmware_url = self.firmware_url + filename
+        self.logger.debug('fw url: %s', firmware_url)
+        r = requests.get(firmware_url)
+        open(dl_file, 'wb').write(r.content)
 
-        unzippeddir = os.path.join(fwdir, 'fw')
         with zipfile.ZipFile(dl_file, 'r') as zip_ref:
-            zip_ref.extractall(unzippeddir)
+            zip_ref.extractall(fwdir)
+        unzippeddir = os.path.join(fwdir, package)
         return unzippeddir
 
 
     def check_modem_type(self):
-        self.logger.info('Confirming modem type')
+        self.logger.warning('Confirming modem type')
         modem_type = self.cloud.network.modem.modem_id
         if modem_type == 'SARA-R410M-02B':
             return True
         raise UpdaterException('Unsupported modem type')
 
     def get_modem_version_digits(self):
-        version = self.cloud.network.modem._basic_command('ATI9')
-        self.logger.info('Got version %s', version)
-        res = re.match(r'^L0\.0\.00\.00\.05\.06,A\.02\.(\d+)$', version)
+        version = self.cloud.network.modem._basic_command('I9')
+        self.logger.warning('Got version %s', version)
+        res = re.match(r'^L0\.0\.00\.00\.05\.0[68],A\.02\.(\d+)$', version)
         if res is None:
             raise UpdaterException('Invalid version string')
         digits = res.group(1)
         return digits
 
     def check_modem_version(self):
-        self.logger.info('Checking current modem version')
+        self.logger.warning('Checking current modem version')
         digits = self.get_modem_version_digits()
         if digits == '04':
             raise UpdaterException('Already latest version')
@@ -154,131 +173,165 @@ class NovaR410Updater(object):
         self.cloud = CustomCloud(None, network='cellular')
 
 
-    def run_update(self, only_checks):
+    def run_update(self, only_checks = False):
         self.init_cloud()
-        self.check_modem_type():
-        version = check_modem_version()
-        if not self.prompt_for_confirm():
-            return False
+        self.check_modem_type()
+        version = self.check_modem_version()
         package_dir = self.download_update_package(version)
+        if only_checks:
+            self.logger.warning('Stopping before applying')
+            return True
         self.apply_update_package(version, package_dir)
+        self.logger.warning('Waiting for install to complete and modem to reconnect')
+        self.logger.warning('This could take 20 minutes. Do not unplug the modem')
+        self.watch_for_stage2_complete()
         self.reprogram_leds()
-        self.logger.info('Done')
+        self.cloud.network.modem.reset()
+        self.logger.warning('Done')
+        return True
 
+    def xgetc(self, size, timeout=1):
+        return self.cloud.network.modem.serial_port.read(size)
+
+    def xputc(self, data, timeout=1):
+        return self.cloud.network.modem.serial_port.write(data)
 
     def send_file(self, filename):
-        self.logger.info('Sending file %s', filename)
-        self.cloud.network.modem.command('+UFWUPD', expected='ONGOING', timeout=1)
+        self.logger.warning('Sending file %s', filename)
+        self.cloud.network.modem.serial_port.write_timeout = 20
+        self.cloud.network.modem.command('+UFWUPD', '3', expected='ONGOING', timeout=60)
+        time.sleep(2)
         fd = open(filename, 'rb')
-        self.cloud.network.modem.serial_port.write(fd.read())
-        self.cloud.network.modem.serial_port.flush()
-        resp = self.cloud.network.modem.serial_port.read()
-        self.logger.info('Response was: %s', resp)
-        if resp[:-2] == 'OK':
-            return True
-        return False
+        self.logger.warning('Writing file to serial port')
+        modem = XMODEM(self.xgetc, self.xputc)
+        sent_success = modem.send(fd, retry=20, timeout=90)
+        fd.close()
+        if not sent_success:
+            raise UpdaterException('Failed to send file via xmodem')
+        self.logger.debug('Done writing')
+        time.sleep(1)
+        return True
 
     def install_loaded_firmware(self):
-        self.cloud.network.modem.set('+UFWINSTALL')
-
+        res, resp = self.cloud.network.modem.command('+UFWINSTALL', timeout=60)
+        if res == 'Error':
+            raise UpdaterException('Firmware Install failed')
+        time.sleep(1)
+        
 
     def apply_update_package(self, version, package_dir):
         for package in self.files[version]:
             packageok = True
+            stagepassed = False
             #stage 1
             for filename in package[0]:
-                self.send_file(filename)
+                fw_file = os.path.join(package_dir, filename)
+                self.send_file(fw_file)
                 self.install_loaded_firmware()
                 res = self.check_for_stage1_return_code()
                 if res == 'OK':
+                    stagepassed = True
                     break
                 elif res == 'STAGEFAIL':
+                    self.logger.warning('File failed. Trying next one in set')
                     continue
                 elif res == 'PACKFAIL':
                     packageok = False
+                    self.logger.warning('Package set failed. Trying next one')
                     break
-            if not packageok:
+            if not packageok or not stagepassed:
                 continue
             #stage 2
             filename = package[1][0]
-            self.send_file(filename)
+            fw_file = os.path.join(package_dir, filename)
+            self.send_file(fw_file)
             self.install_loaded_firmware()
-            self.watch_for_stage2_complete()
+            return
+        # looped through everything without success
+        raise UpdaterException('Was unable to install any update package successfully')
 
 
     def check_for_stage1_return_code(self):
-        self.logger.info('Waiting for stage1 return code')
-        self.cloud.network.modem.serial_port.timeout = 60
-        while True:
-            r = self.cloud.network.modem.serial_port.readline()
-            if not r:
-                continue
-            r = r.rstrip('\r\n')
-            self.logger.debug('Read from port: %s', r)
-            fwstatus = re.match(r'+UFWSTATUS: (\w+), (\w+), (\w)', r)
-            if not fwstatus:
-                continue
-            flag, error_code, failure_reason = fwstatus.group(1, 2, 3)
-            if flag.upper() == '55436F6D':
-                # succeeded
-                return 'OK'
-            elif flag == '55457272':
-                if error_code == '19a':
-                    if failure_reason == 'ffe3':
-                        return 'STAGEFAIL'
-                    elif failure_reason == 'ffed':
-                        return 'PACKFAIL'
-                    else:
-                        raise UpdaterException('Unknown failure reason %s',
-                                failure_reason)
+        self.logger.warning('Waiting for stage1 return code')
+        self.wait_for_modem(61)
+        result, response = self.cloud.network.modem.command('+UFWSTATUS?')
+        fwstatus = re.match(r'\+UFWSTATUS: (\w+), (\w+), (\w+)', response)
+        if not fwstatus:
+            raise UpdaterException('Invalid UFWSTATUS response', fwstatus)
+        flag, error_code, failure_reason = fwstatus.group(1, 2, 3)
+        if flag.upper() == '55436F6D':
+            # succeeded
+            return 'OK'
+        elif flag == '55457272':
+            if error_code == '19a':
+                if failure_reason == 'ffe3':
+                    return 'STAGEFAIL'
+                elif failure_reason == 'ffed':
+                    return 'PACKFAIL'
                 else:
-                    raise UpdaterException('Unknown error code %s', error_code)
+                    raise UpdaterException('Unknown failure reason',
+                            failure_reason)
             else:
-                raise UpdaterException('Unknown flag %s', flag)
+                raise UpdaterException('Unknown error code', error_code)
+        else:
+            raise UpdaterException('Unknown flag', flag)
 
 
     def reprogram_leds(self):
         self.cloud.network.modem.command('+UGPIOC', '23,10')
         self.cloud.network.modem.command('+UGPIOC', '16,2')
 
-    def wait_for_modem(self):
-        while True:
+    def wait_for_modem(self, maxtime):
+        self.logger.warning('Waiting for modem')
+        stop_at = time.time() + maxtime
+        while time.time() < stop_at:
             self.cloud = None
             try:
                 self.init_cloud()
-            except NetworkError as e:
-                time.sleep(10)
+            except Exception as e:
+                time.sleep(30)
+                self.logger.warning(
+                        'Still waiting for modem to finish install. Do not unplug')
                 continue
             break
+        if self.cloud is None:
+            raise UpdaterException('Failed to detect modem after maximum time')
 
     def watch_for_stage2_complete(self):
         # We should see the usb and serial ports go away while the install is
         # running so we watch for them to come back up and then run ATI9 to
         # confirm version is updated
-        self.wait_for_modem()
+        self.logger.info('Waiting for stage 2 install to finish. Could be 22 minutes')
+        self.wait_for_modem( (60*22) )
         digits = self.get_modem_version_digits()
         if digits == '04':
             return
         else:
-            raise UpdaterException('Got unexpected modem version %s', digits)
+            raise UpdaterException('Got unexpected modem version', digits)
 
 
 def main():
     logger = logging.getLogger('')
+    logger.setLevel(logging.DEBUG)
     sh = logging.StreamHandler()
     sh.setFormatter(logging.Formatter('%(message)s'))
-    sh.setLevel(logging.INFO)
+    sh.setLevel(logging.WARNING)
     logger.addHandler(sh)
     fh = logging.FileHandler('novaupdater.log')
     fh.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
     fh.setLevel(logging.DEBUG)
     logger.addHandler(fh)
+    logger.debug('Started')
 
     upd = NovaR410Updater()
     if not upd.prompt_for_confirm():
         sys.exit(0)
-    upd.run_update()
-    print('Update Complete\n')
+    try:
+        upd.run_update()
+    except UpdaterException as e:
+        logger.error('ERROR: '+str(e))
+    else:
+        print('Update Complete\n')
 
 
 if __name__ == '__main__':
